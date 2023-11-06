522c4b36a3f83396685c40e3a7030ccf536b2357
DamienChailley <playwithcube@gmail.com>
2023-11-07 00:09:38 +0100

    Handle Comparison, implement new method in rules

--- PokerHands/Rules/HighCardRule.cs
+++ PokerHands/Rules/HighCardRule.cs
@@ -2,7 +2,7 @@
 
 namespace PokerHands.Rules
 {
-    // Like that, we can as many rules as we want without modifying the existing code.
+    // With the following technique, we can as many rules as we want without modifying the existing code.
     public class HighCardRule : IRule
     {
         public RuleResult GetRuleResult(string[] hand)
@@ -10,22 +10,46 @@ namespace PokerHands.Rules
             int bestCardLevel = -1;
             string bestCardName = string.Empty;
 
-            foreach(string card in hand)
+            foreach (string card in hand)
             {
                 int level = GameConstants.Grades.IndexOf(card[0].ToString());
 
-                if (level > bestCardLevel) {
+                if (level > bestCardLevel)
+                {
                     // we found a new best card.
                     bestCardLevel = level;
                     bestCardName = card;
                 }
             }
 
-            return new() {
+            return new()
+            {
                 Composition = Composition.HighCard,
                 Grades = new string[1] { bestCardName[0].ToString() }
             };
 
+        }
+
+        public WinningState GetFirstPlayerState(string[] firstHand, string[] secondHand)
+        {
+            RuleResult firstPlayerResult = GetRuleResult(firstHand);
+            RuleResult secondPlayerResult = GetRuleResult(secondHand);
+
+            int firstPlayerIndex = GameConstants.Grades.IndexOf(firstPlayerResult.Grades[0]);
+            int secondPlayerIndex = GameConstants.Grades.IndexOf(secondPlayerResult.Grades[0]);
+
+            if (firstPlayerIndex > secondPlayerIndex)
+            {
+                return WinningState.Win;
+            }
+            else if (firstPlayerIndex < secondPlayerIndex)
+            {
+                return WinningState.Win;
+            }
+            else
+            {
+                return WinningState.Tie;
+            }
         }
     }
 }
--- PokerHands/RulesService.cs
+++ PokerHands/RulesService.cs
@@ -3,13 +3,30 @@
     internal class RulesService : IRulesService
     {
         /** Rules of the game **/
-        private readonly List<IRule> _rules = new();
+        private List<IRule> _rules = new();
 
         public RulesService() { }
 
         public void AddRuleToRules(IRule rule)
         {
             _rules.Add(rule);
+        }
+
+        public RuleResult ComputesHand(string[] hand)
+        {
+            _rules.Reverse(); // get the most important rules first..
+
+            foreach (var rule in _rules)
+            {
+                RuleResult ruleResult = rule.GetRuleResult(hand);
+                if (ruleResult.Composition != Composition.None)
+                {
+                    return ruleResult; // returns the first ruleResult that isn't None..
+                }
+            }
+            _rules.Reverse(); // a bit dirty..
+
+            return new() { Composition = Composition.None }; // Not possible, exepted if we didn't add rules.
         }
     }
 }
