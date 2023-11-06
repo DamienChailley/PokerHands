7cf51737451fb90d2ab3ba8c81a6a916c2e4aa32
DamienChailley <playwithcube@gmail.com>
2023-11-06 20:55:14 +0100

    Initial commit

---
+++ PokerHandsUnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace PokerHandsUnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
